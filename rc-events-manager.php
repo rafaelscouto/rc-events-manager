6c9225d93c6f742859868252685b6ad94fafbabd
Rafael Couto <rafael.couto@contrato.g4f.com.br>
2024-03-10 23:41:33 -0300

    Initial commit

---
+++ rc-events-manager.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: RC Events Manager
+ * Plugin URI: https://github.com/rafaelscouto/rc-events-manager
+ * Description: A complete solution for managing events and issuing certificates in WordPress.
+ * Author: Rafael Couto
+ * Author URI: https://rafaelscouto.com.br
+ * Version: 1.0
+ * License: GPLv3
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain: rc-events-manager
+ * Domain Path: /src/languages
+ */
+
+if ( ! defined( 'ABSPATH' ) ){
+    exit;
+}
